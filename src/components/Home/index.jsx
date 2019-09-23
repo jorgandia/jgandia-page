8dbdbeb64d0523e836a98deacdc1c28694852445
jorgandia <jorgancer@gmail.com>
2019-09-23 07:42:41 +0200

    Home restyled

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,5 +1,6 @@
-import React, {useState, useEffect} from 'react';
-import { Container, Grid, Typography } from '@material-ui/core';
+import React, {useState, useLayoutEffect} from 'react';
+import { Container, Grid } from '@material-ui/core';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 
 import { GithubIcon, LinkedinIcon, MailIcon } from '../../icons';
 import Flip from 'react-reveal/Flip';
@@ -8,32 +9,40 @@ import './style.scss';
 
 const Home = () => {
     
-    const wordtest = ['Hola, soy', "Hello, I'm", "Hallo, ich bin", "Ciao, sono", "Salut! Je suis", "Oi! Sou"];
+   const wordtest = ["Hola, soy", "Hola, sóc", "Hallo, ich bin", "Ciao, sono", "Salut! Je suis", "Oi! Sou", "Hello, I'm"];
 
-    const [word, setWord] = useState(wordtest[0]);
 
-    const handleWordChange = () => {
-        setTimeout(() => {setWord(
-            word => word = wordtest[Math.floor(Math.random()*wordtest.length)],
-        )}, 5000);
-        console.log(word);
-        return (<Flip top>{word}</Flip>);
+   const [word, setWord] = useState(wordtest[0]);
+
+   const underlined = word==="Hello, I'm" ? true : false;
+   const bigScreen = useMediaQuery('(min-width: 851px)');
+   const mediumScreen = useMediaQuery('(min-width: 651px) and (max-width: 850px)')
+   const smallScreen = useMediaQuery('(max-width: 650px)');
+
+   const handleScreenSize = () => {
+     if (mediumScreen){
+            return "home-container__title medium-screen";
+    } else if (smallScreen){
+            return "home-container__title small-screen";
+    } else if (bigScreen){
+        return "home-container__title big-screen";
     }
+   };
 
-    
-    // useEffect(() =>{
-    //     setTimeout(() => {setWord(
-    //         word => word = wordtest[Math.floor(Math.random()*wordtest.length)],
-    //     )}, 3000);
-    // }, [word])
-    
+    useLayoutEffect (() =>{
+        wordtest.forEach((value, index)=>{
+            setTimeout(()=>
+            {setWord(value)}, index * 2000)
+            });
+        }, [])
+
     return (
         <Container className="home-container">
-                <Grid>
-                    <Grid className="home-container__title">{handleWordChange()} Jordi Gandía</Grid>
+                <Grid className={ underlined ? "definitive-underLine" : "underline"}>
+                   <Grid className={handleScreenSize()}><Flip top key={word}>{word}</Flip> Jordi Gandía</Grid>
                 </Grid>
                 <Grid className="home-container__subtitle">
-                    <Typography variant="h5">web Developer</Typography>
+                    Web Developer
                 </Grid>
                 <Grid className="home-container__icons-container">
                     <a target="blank" href="https://www.linkedin.com/in/jordi-gandia-cerda/"><LinkedinIcon className="home-container__icons-container--icon"/></a>
