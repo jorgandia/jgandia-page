93c18c826e532929307d59e35d5ed8402bc6bbfc
jorgandia <jorgancer@gmail.com>
2020-02-10 20:59:19 +0100

    Adding particles library

---
+++ src/components/ParticlesComponent.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Particles from "react-particles-js";
+import { params } from './particleParams'
+
+const ParticlesComponent = () => {
+    return (
+        <div
+    style={{
+      position: "absolute",
+      top: 0,
+      left: 0,
+      width: "100%",
+      height: "100%"
+    }}
+  >
+    <Particles
+      params={params}
+    />
+  </div>
+  )
+}
+
+export default ParticlesComponent;
